726a139ed8eaf839abc9ff3016ed99b205b5099b
seijun <seijun.chung@gmail.com>
2022-02-16 23:10:56 +0900

    revisions in ticc commit

--- src/ticc.py
+++ src/ticc.py
@@ -30,6 +30,7 @@ data_sensor = []
 data_bay = []
 data_pmmode = []
 sensor_cols_merge = []
+
 for num_case in case:
     num_case = num_case - 1
     if dataset == 'dataset2':
@@ -47,9 +48,7 @@ for num_case in case:
     # sensors = df.loc[:, df.columns.str.contains('Bay_NH3')]
     pm_mode_cols = list(df.columns[df.columns.str.contains('PM Mode')])
     pm_mode = df[pm_mode_cols]
-    pm_mode = pm_mode.replace({False: 0,
-                               True: 1,
-                               np.nan: 0})
+    pm_mode = pm_mode.replace({False: 0, True: 1, np.nan: 0})
     # resampling
     # if resample_interval != None:
     #     df = df.resample(resample_interval).median()
@@ -65,12 +64,12 @@ for num_case in case:
     pm_mode_temp = pm_mode.iloc[:, 0]
 
     # plot data samples
-    fig, ax = plt.subplots(figsize=(20, 5))
-    trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
-    ax.fill_between(pm_mode.index.values, 0, 1, where=pm_mode.sum(axis=1) != 0,
-                    facecolor='r', alpha=0.3, transform=trans)
-    ax.plot(df, alpha=0.8, linewidth=0.5)
-    plt.show()
+    # fig, ax = plt.subplots(figsize=(20, 5))
+    # trans = mtransforms.blended_transform_factory(ax.transData, ax.transAxes)
+    # ax.fill_between(pm_mode.index.values, 0, 1, where=pm_mode.sum(axis=1) != 0,
+    #                 facecolor='r', alpha=0.3, transform=trans)
+    # ax.plot(df, alpha=0.8, linewidth=0.5)
+    # plt.show()
 
     # concatenate data
     data_sensor.append(df[sensor_cols])
@@ -103,12 +102,12 @@ scaler.fit(df_sensor)
 df_sensor = scaler.transform(df_sensor)
 
 # plot data samples
-fig1, ax1 = plt.subplots(figsize=(20, 5))
-trans = mtransforms.blended_transform_factory(ax1.transData, ax1.transAxes)
-ax1.fill_between(df_pmmode.index.values, 0, 1, where=df_pmmode.sum(axis=1) != 0,
-                facecolor='r', alpha=0.3, transform=trans)
-ax1.plot(df_sensor, alpha=0.8, linewidth=0.5)
-plt.show()
+# fig1, ax1 = plt.subplots(figsize=(20, 5))
+# trans = mtransforms.blended_transform_factory(ax1.transData, ax1.transAxes)
+# ax1.fill_between(df_pmmode.index.values, 0, 1, where=df_pmmode.sum(axis=1) != 0,
+#                 facecolor='r', alpha=0.3, transform=trans)
+# ax1.plot(df_sensor, alpha=0.8, linewidth=0.5)
+# plt.show()
 
 # save txt
 # np.savetxt('output_folder/Data_sensor_{}_{}case_{}.txt'.format(dataset,case,resample_interval), df_sensor, fmt='%d', delimiter=',')
@@ -121,13 +120,13 @@ intervals = 3800
 w = 10
 
 # plot data samples
-fig2, ax2 = plt.subplots(figsize=(10,5))
-trans = mtransforms.blended_transform_factory(ax2.transData, ax2.transAxes)
-ax2.fill_between(df_pmmode.index.values[intervals:intervals+w], 0, 1, where=df_pmmode[intervals:intervals+w].sum(axis=1) !=0,
-                facecolor='r', alpha=0.3, transform=trans)
-ax2.plot(df_sensor[intervals:intervals+w], alpha=0.8, linewidth=0.5)
-fig2.suptitle("resample=60min, window size=10 steps")
-plt.show()
+# fig2, ax2 = plt.subplots(figsize=(10,5))
+# trans = mtransforms.blended_transform_factory(ax2.transData, ax2.transAxes)
+# ax2.fill_between(df_pmmode.index.values[intervals:intervals+w], 0, 1, where=df_pmmode[intervals:intervals+w].sum(axis=1) !=0,
+#                 facecolor='r', alpha=0.3, transform=trans)
+# ax2.plot(df_sensor[intervals:intervals+w], alpha=0.8, linewidth=0.5)
+# fig2.suptitle("resample=60min, window size=10 steps")
+# plt.show()
 
 BOCD = {'case': [1,8,9,11,14,4],
         'dataset' : ['dataset1','dataset1','dataset1','dataset1','dataset1','dataset2'],
@@ -140,7 +139,7 @@ BOCD = {'case': [1,8,9,11,14,4],
         'start': [40000,0,41897,62842,62842,31704],
         'end': [45000,20948,62846,83789,83789,42272]}
 
-max_clusters = [3, 4]
+max_clusters = [3, 4, 5]
 window_size = 5  # 1
 maxIters = 8  # 100      ## number of Iterations of the smoothening
 beta = 400  # 600                 ## smoothness parameter
@@ -164,6 +163,7 @@ for itt in range(6):
             fname = file_list[num_case + 24]
         else:
             fname = file_list[num_case]
+
         print(fname)
         df = pd.read_csv(fname, low_memory=False)
 
@@ -242,10 +242,8 @@ for itt in range(6):
         (cluster_assignment, cluster_MRFs, bic) = ticc.fit(input_file=fname)
 
         np.savetxt(
-            'output_folder/Results_{}_{}case_w={}_{}clusters_beta{}_lambda{}_{}_example_{}th_specific_region_.txt'  # _includingbay_scaler
-            .format(dataset, case, window_size, num_clusters, beta, lambda_parameter, resample_interval, itt),
+            '../results/models/ticc_results/Results_{}_{}case_w={}_{}clusters_beta{}_lambda{}_{}_example_{}th_specific_region_.txt'.format(dataset, case, window_size, num_clusters, beta, lambda_parameter, resample_interval, itt),
             cluster_assignment, fmt='%d', delimiter=',')
-        print(bic)
         print("bic/{}cluster: {}".format(num_clusters, bic))
 
         # plot
@@ -259,16 +257,14 @@ for itt in range(6):
         for k in range(num_clusters):
             df_sensor_temp = fname.copy()
             df_sensor_temp = df_sensor_temp  # pd.DataFrame(np.nan_to_num(df_sensor_temp))
-            df_sensor_temp.loc[np.where(cluster_assignment == k)[0]]
             df_sensor_temp.loc[np.where(cluster_assignment != k)[0]] = np.nan
             axs[t].plot(df_sensor_temp.fillna(0), c=cmaps[k], alpha=0.5, linewidth=1.)
             axs[t].set_title('# of clusters = {}'.format(num_clusters))
-        #             plt.xticks(idx_orig, idx_val)
         t += 1
 
     fig.tight_layout()
     plt.savefig(
-        'output_folder/Results_{}_{}case_w={}_beta{}_lambda{}_{}_example_{}th_specific_region_.png'.format(dataset,
+        '../results/plots/ticc/Results_{}_{}case_w={}_beta{}_lambda{}_{}_example_{}th_specific_region_.png'.format(dataset,
                                                                                                            case,
                                                                                                            window_size,
                                                                                                            beta,
@@ -280,14 +276,14 @@ for itt in range(6):
 # load results and plot each number of clusters
 dataset = 'dataset1'
 case = [8]
-window_size, max_clusters, beta, lambda_parameter = 5, [3, 4], 400, 11e-2
+window_size, max_clusters, beta, lambda_parameter = 5, [3, 4, 5], 400, 11e-2
 resample_interval = None  # '60T' # T=min
 BOCD = [41265, 41288, 41860, 43020]
 
 t = 0
 fig, axs = plt.subplots(len(max_clusters), 1, figsize=(10, 3 * 3))
 for num_clusters in max_clusters:
-    input_file = 'output_folder/Results_{}_{}case_w={}_{}clusters_beta{}_lambda{}_{}_example_{}th_specific_region_.txt'.format(
+    input_file = 'results/models/ticc_results/Results_{}_{}case_w={}_{}clusters_beta{}_lambda{}_{}_example_{}th_specific_region_.txt'.format(
         dataset, case, window_size, num_clusters, beta, lambda_parameter, resample_interval, itt)
     #     df_sensor_file = 'output_folder/Data_sensor_{}_{}case_{}_includingbay.txt'.format(dataset,case,resample_interval) #_includingbay_scaler
     #     df_pmmode_file = 'output_folder/Data_pmmode_{}_{}case_{}.txt'.format(dataset,case,resample_interval)
@@ -310,12 +306,11 @@ for num_clusters in max_clusters:
                         facecolor='r', alpha=0.2, transform=trans)
     for k in range(num_clusters):
         df_sensor_temp = df_sensor.copy()
-        df_sensor_temp.loc[np.where(cluster_assignment == k)[0]]
         df_sensor_temp.loc[np.where(cluster_assignment != k)[0]] = np.nan
         axs[t].plot(df_sensor_temp[40000:45000], c=cmaps[k], alpha=0.5, linewidth=1.)
         axs[t].set_title('# of clusters = {}'.format(num_clusters))
     t += 1
 plt.tight_layout()
-plt.savefig('output_folder/Results_{}_{}case_w={}_beta{}_lambda{}_{}_example_{}th_specific_region.png'.format(dataset, case, window_size, beta,
+plt.savefig('results/plots/ticc/Results_{}_{}case_w={}_beta{}_lambda{}_{}_example_{}th_specific_region.png'.format(dataset, case, window_size, beta,
                                                                                  lambda_parameter, resample_interval, itt))
 plt.show()
